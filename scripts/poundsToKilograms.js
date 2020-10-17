885c2481a7121f263f8a5265af38772d04021320
LadyUnicornRobot <sarahebawden@gmail.com>
2020-10-16 20:26:03 -0600

    format

--- scripts/poundsToKilograms.js
+++ scripts/poundsToKilograms.js
@@ -1,12 +1,12 @@
 //name function
-function convertPounds(){
+function convertPounds() {
 
     //name variable, get input, parseFloat processing
-    let pounds = parseFloat(document.getElementById("pounds").value);  
+    let pounds = parseFloat(document.getElementById("pounds").value);
 
     //processing, convert to kg
     let output = pounds * 0.45359237;
-   
+
     //output, processing to one decimal
     document.getElementById("kilograms").innerHTML = output.toFixed(1) + " kilograms.";
 
