9be21e8479b43c1761e10f0d8d0f724214b5ce69
LadyUnicornRobot <sarahebawden@gmail.com>
2020-11-14 11:48:56 -0700

    finished sumOdds

--- scripts/sumOdds.js
+++ scripts/sumOdds.js
@@ -5,27 +5,16 @@ function addOddIntegers() {
 
     let n = parseFloat(document.getElementById('userNumber').value);
 
+    //make sure I have the right input and the right output
+    //need variable 'sum' to hold the output
+    let sum = 0;
 
 
-    for (i = 1; i <= n; i += 2) {
-        
+    for (let i = 1; i <= n; i += 2) {
+        sum += i;
     }
-// can't figure out how to get it to add up the integers.
-
 
     // output sum to user in #oddSum
 
-    document.getElementById('oddSum').innerHTML = "The sum of the odd integers of " + userNumber + "is " + i + "."
+    document.getElementById('oddSum').innerHTML = "The sum of the odd integers of " + n + " is " + sum + "."
 }
-/* notes:
-    /* while (i <= n) {
-        if (i % 2 !== 0) { //check for oddness
-            i += n; // change value of i
-        }
-    /* find odd integers by n % 2
-    * between 1 and n, including n
-    * add odd integers together
-    */
-
-    // what do I need to repeat? I need to check if the number is odd and if it is then add it to the total, or in other words change n. Figure out the numbers less than or equal to n.
-    }
