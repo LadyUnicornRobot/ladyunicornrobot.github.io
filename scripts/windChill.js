bffa7e328981471b6082f50d0df578f7ea6b8d95
LadyUnicornRobot <sarahebawden@gmail.com>
2020-11-21 23:45:20 -0700

    windchill

---
+++ scripts/windChill.js
@@ -0,0 +1,33 @@
+function doInputOutput() {
+/* called from onClick
+* get temperature and store in var temperature
+* get wind speed and store in var windspeed
+* call second function windchill() if both inputs meet specifications
+* store value returned by windchill() in a var windchill
+* output value returned by windchill() to div for user to see
+*/
+let temperature = parseFloat(document.getElementById("temperature").value);
+let windspeed = parseFloat(document.getElementById("windspeed").value);
+
+if (temperature > 50 || windspeed < 3) {
+    document.getElementById("output").innerHTML = "Not able to compute windchill. Temperature too high or windspeed too low.";
+}   else {
+    let windchill = windChill(temperature, windspeed);
+document.getElementById("output").innerHTML = "Windchill is " + windchill.toFixed(2) + "&deg;F. 🥶";
+}
+
+
+}
+
+function windChill(tempF, speed) {
+/* take a temperature in F as parameter
+* take wind speed in mph as parameter
+* calculate wind chill factor as temp F
+* return wind chill factor in F
+* NO input NO output, only calculate and return result
+*/
+let t = tempF;
+let s = speed;
+let f = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t) * (s ** 0.16);
+return f;
+}
