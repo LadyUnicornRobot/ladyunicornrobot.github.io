0eb6f71d5e715dd6f2ee9e8ffaa41494afb696a9
LadyUnicornRobot <sarahebawden@gmail.com>
2021-01-09 19:02:18 -0700

    format

--- WDD230/js/assignmentPortal.js
+++ WDD230/js/assignmentPortal.js
@@ -1,3 +1,4 @@
 let d = new Date();
 document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
-document.getElementById("lastmodified").innerHTML = "Last Modified: " + document.lastModified;
+document.getElementById("lastmodified").innerHTML =
+  "Last Modified: " + document.lastModified;
