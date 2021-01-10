439505c7606201205d82af1b41aef36e439824dc
LadyUnicornRobot <sarahebawden@gmail.com>
2021-01-09 18:58:02 -0700

    assignment portal finished

---
+++ WDD230/js/assignmentPortal.js
@@ -0,0 +1,3 @@
+let d = new Date();
+document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
+document.getElementById("lastmodified").innerHTML = "Last Modified: " + document.lastModified;
