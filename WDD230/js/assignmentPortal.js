a681fd76204d61c0f164ece417fed2849799d6a3
LadyUnicornRobot <sarahebawden@gmail.com>
2021-04-21 20:46:55 -0600

    start of semester

--- WDD230/js/assignmentPortal.js
+++
@@ -1,4 +0,0 @@
-let d = new Date();
-document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
-document.getElementById("lastmodified").innerHTML =
-  "Last Modified: " + document.lastModified;
--- WDD230/js/portal.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("lastModified").innerHTML = document.lastModified
-
-
-let d = new Date();
-document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
