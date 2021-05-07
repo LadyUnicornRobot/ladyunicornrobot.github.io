8e49b453c4a7c181deedab1bc19e907962bbff23
LadyUnicornRobot <sarahebawden@gmail.com>
2021-05-07 17:58:17 -0600

    w03websiteplanning

---
+++ WDD230/lesson3/js/website-planning-document.js
@@ -0,0 +1,4 @@
+let d = new Date();
+document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
+document.getElementById("lastmodified").innerHTML =
+  "Last Modified: " + document.lastModified;
