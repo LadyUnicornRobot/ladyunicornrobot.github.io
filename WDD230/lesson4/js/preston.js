eb96ed3324f285fc1a3e153b9172c2cb1dbe0b9e
LadyUnicornRobot <sarahebawden@gmail.com>
2021-05-15 11:53:18 -0600

    week04prestonhomepage

---
+++ WDD230/lesson4/js/preston.js
@@ -0,0 +1,14 @@
+let d = new Date();
+document.getElementById("copyright").innerHTML = "&copy;" + d.getFullYear();
+
+let c = new Date(day, date, month, year)
+document.getElementById("currentdate").innerHTML = c;
+
+/* date display format: Day, 01 Month 2021
+
+* create another function to assign day of the week names to number of day
+* how can I string these all together without actually adding the values?
+* can I use one function and put something in parentheses?
+* grrr... I can't remember how to do JavaScript. :'(
+
+*/
